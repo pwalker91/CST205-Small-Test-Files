4b7ffebd30c9211ca8c4ff10cdc1245d08ada28e
pwalker91 <pwalker@csumb.edu>
2014-03-03 15:56:31 -0800

    Added String Fun and test.py
    String Fun prints out the last half of a string backwards

---
+++ test.py
@@ -0,0 +1,10 @@
+"""
+n = raw_input("Input your name, nerd: ")
+print "hi"
+if (n=="Peter"):
+    print "well aren't you special"
+else:
+    print "I don't know you"
+"""
+for i in range(10):
+  printNow(i)
